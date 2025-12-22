fe525042c87813bd402fb13359750606d743fc7e
Meyllane <vilmora.ser@tuta.com>
2025-12-22 21:04:39 +0100

    Formalized the structure of each RamUnit and added a function to get the biggest server

--- src/lib/rammap.ts
+++ src/lib/rammap.ts
@@ -1,11 +1,35 @@
 import { NS } from "@ns"
 
-export class RamMap {
-    public map: {serverName: string, ram: number}[]
+export class RamUnit {
+    public hostname: string
+    public availableRam: number
 
-    public constructor(ns: NS, serversList: string[]) {
-        this.map = serversList.map((serverName) => {
-            return {serverName: serverName, ram: ns.getServerMaxRam(serverName) - ns.getServerUsedRam(serverName)}
+    public constructor(hostname: string, availableRam: number) {
+        this.hostname = hostname
+        this.availableRam = availableRam
+    }
+}
+
+export class RamMap {
+    public ns: NS
+    public map: RamUnit[]
+
+    public constructor(ns: NS, hostList: string[]) {
+        this.ns = ns
+        this.map = hostList.map((hostname) => {
+            return new RamUnit(
+                hostname,
+                this.ns.getServerMaxRam(hostname) - this.ns.getServerUsedRam(hostname)
+            )
         })
+    }
+
+    public getBiggestServer(excludeHome: boolean) {
+        let map = this.map
+        if (excludeHome) map = map.filter((server) => server.hostname != 'home')
+
+        if (map.length == 0) return null
+        
+        return map.sort((a, b) => b.availableRam - a.availableRam)[0]
     }
 }
