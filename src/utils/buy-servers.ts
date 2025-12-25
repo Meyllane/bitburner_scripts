d95fe4bbd1203b6f35d7b2e64a09b4b67e996f9a
Meyllane <vilmora.ser@tuta.com>
2025-12-25 12:58:03 +0100

    Added a server buyer class than handles Fleet management

--- src/utils/buy-servers.ts
+++ src/utils/buy-servers.ts
@@ -1,54 +1,130 @@
-import { NS } from "@ns";
+import { customPrint } from "@/lib/func";
+import { NS, Server } from "@ns";
+
+export class FleetManager {
+    private ns: NS
+    public serverLimit: number
+    public serverList: string[]
+    public percMoneyAvailable: number = 0.6
+    private moneyAvaiable;
+    private serverBuyCost: {ram: number, price: number}[]
+    public fleetFullyMaxed: boolean = false
+
+    public constructor(ns: NS){
+        this.ns = ns
+        this.serverLimit = this.ns.getPurchasedServerLimit()
+        this.serverList = this.ns.getPurchasedServers()
+        this.moneyAvaiable = ns.getPlayer().money * this.percMoneyAvailable
+
+        this.serverBuyCost = this.getServerBuyCost()
+    }
+
+    private getServerBuyCost() {
+        let res = []
+        for (let i=1; i<=20; i++) {
+            res.push({
+                ram: 2**i,
+                price: this.ns.getPurchasedServerCost(2**i)
+            })
+        }
+
+        return res
+    }
+
+    public updateServerList() {
+        this.serverList = this.ns.getPurchasedServers()
+    }
+
+    public getBiggestNonMaxedServer() {
+        let servers = this.serverList
+            .map(host => {return {host: host, ram: this.ns.getServerMaxRam(host)}})
+            .filter(server => server.ram != 2**20)
+            .sort((a, b) => b.ram - a.ram)
+        
+        if (servers.length == 0) return undefined
+
+        return servers[0].host
+    }
+
+    public buyServer() {
+        let buyOrders = this.serverBuyCost
+            .filter(order => order.price <= this.moneyAvaiable)
+            .sort((a, b) => b.price - a.price)
+
+        if (buyOrders.length == 0) return false
+
+        let serverName = `server-${this.serverList.length}`
+        let order = buyOrders[0]
+        
+        customPrint(this.ns, `Bought ${serverName} with ${this.ns.formatRam(order.ram)} for ${this.ns.formatNumber(order.price)}$.`)
+        this.ns.purchaseServer(serverName, order.ram)
+        this.moneyAvaiable -= order.price
+
+        return true
+    }
+
+    public upgradeServer(hostname: string) {
+        let buyOrders = []
+
+        for (let i = 1; i<=20; i++) {
+            let order = {
+                ram: 2**i,
+                price: this.ns.getPurchasedServerUpgradeCost(hostname, 2**i)
+            }
+
+            if (order.price == -1) continue
+            if (order.price > this.moneyAvaiable) continue
+
+            buyOrders.push(order)
+        }
+
+        if (buyOrders.length == 0) return false
+
+        buyOrders = buyOrders
+            .sort((a, b) => b.ram - a.ram)
+        
+        let order = buyOrders[0]
+
+        customPrint(this.ns, `Upraded ${hostname} with ${this.ns.formatRam(order.ram)} for ${this.ns.formatNumber(order.price)}$`)
+        this.ns.upgradePurchasedServer(hostname, order.ram)
+        this.moneyAvaiable -= order.price
+
+        return true
+    }
+
+    public run() {      
+        if (this.serverList.length == 0) {
+            this.buyServer()
+            this.updateServerList()
+        }
+        
+        while (true) {
+            let server = this.getBiggestNonMaxedServer()
+
+            if (server == undefined && this.serverList.length == this.serverLimit) {
+                this.fleetFullyMaxed = true
+                break
+            }
+
+            //Buy new
+            if (server == undefined) {
+                let succes = this.buyServer()
+                if (!succes) break
+                this.updateServerList()
+                continue
+            }
+
+            //Upgrade
+            if (!this.upgradeServer(server)) break
+
+            this.updateServerList()
+        }
+    }
+}
 
 export async function main(ns: NS) {
-    let money = ns.getPlayer().money
+    ns.disableLog("ALL")
+    const FLEET_MANAGER = new FleetManager(ns)
 
-    const SERVER_LIMIT = ns.getPurchasedServerLimit()
-    const SERVER_PRICE = [...Array(20).keys()]
-        .map((ramIndex) => ramIndex + 1)
-        .map((ramIndex) => {return {ram:2**ramIndex, price:ns.getPurchasedServerCost(2**ramIndex)}})
-
-    while (true) {
-        let nb_servers = ns.getPurchasedServers().length
-
-        if (nb_servers < SERVER_LIMIT) {
-            let options = SERVER_PRICE
-                .filter((server) => server.price <= money && server.ram >= 64)
-                .sort((a, b) => b.price - a.price)
-            
-            if (options.length == 0) {
-                ns.tprint("No good options for servers.")
-                ns.exit()
-            }
-
-            nb_servers = ns.getPurchasedServers().length
-
-            let serverName = `server-${nb_servers}`
-
-            ns.purchaseServer(serverName, options[0].ram)
-            ns.tprint(`Bought ${serverName} with ${ns.formatRam(options[0].ram)} RAM`)
-            money -= options[0].price
-
-            ns.run("utils/scp.js")
-
-        } else {
-            const SMALLEST_SERVER = ns.getPurchasedServers()
-                .map((server) => ns.getServer(server))
-                .sort((a, b) => a.maxRam - b.maxRam)[0]
-            
-            let options = SERVER_PRICE
-                .filter((server) => server.price <= money && server.ram >= 64 && server.ram > SMALLEST_SERVER.maxRam)
-                .sort((a, b) => b.price - a.price)
-
-            if (options.length == 0) {
-                ns.tprint("No good options for servers.")
-                ns.exit()
-            }
-
-            ns.upgradePurchasedServer(SMALLEST_SERVER.hostname, options[0].ram);
-            ns.tprint(`Upgraded ${SMALLEST_SERVER.hostname} with ${ns.formatRam(options[0].ram)} RAM`)
-            money -= options[0].price
-        }
-    } 
-
+    FLEET_MANAGER.run()
 }
