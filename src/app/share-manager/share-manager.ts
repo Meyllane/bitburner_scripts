1378c5dc2f82b1d01a4f1e083a9ab4f87cbfbbed
Meyllane <vilmora.ser@tuta.com>
2025-12-25 14:33:26 +0100

    Fixer ram cost evaluation problem

--- src/app/share-manager/share-manager.ts
+++ src/app/share-manager/share-manager.ts
@@ -1,6 +1,6 @@
 import { NS } from "@ns";
 
-const SHARE_COST = 2.5
+const SHARE_COST = 4
 
 export async function main(ns: NS) {
     let workers = ["server-0"]
@@ -9,13 +9,18 @@ export async function main(ns: NS) {
 
     ns.atExit(() => pids.forEach(ns.kill))
 
-    for (let server of workers) {
-        let MAX_SHARE = Math.floor(ns.getServerMaxRam(server) / SHARE_COST )
-        let pid = ns.exec(
-            "app/share-manager/share.js",
-            server,
-            {ramOverride: 2.5, threads: MAX_SHARE}
-        )
-        pids.push(pid)
+    while (true) {
+        for (let server of workers) {
+            let MAX_SHARE = Math.floor(ns.getServerMaxRam(server) / SHARE_COST)
+            if (MAX_SHARE == 0) continue
+            let pid = ns.exec(
+                "app/share-manager/share.js",
+                server,
+                {ramOverride: SHARE_COST, threads: MAX_SHARE}
+            )
+            pids.push(pid)
+        }
+
+        await ns.sleep(100)
     }
 }
--- src/app/share-manager/share.ts
+++ src/app/share-manager/share.ts
@@ -1,5 +1,7 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS) {
-    await ns.share()
+    while (true) {
+        await ns.share()
+    }
 }
